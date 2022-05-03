df9f8dbb88308036c23e507f86d185e6ae0e3b2f
Mario Claudius <mcmario7901@gmail.com>
2022-05-04 00:18:06 +0700

    modify yaml file (13)

---
+++ src/client.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg');
+
+const pgclient = new Client({
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+});
+
+pgclient.connect();
