0b58003b945459f6762750a358290ed40b34c6c7
Mario Claudius <mcmario7901@gmail.com>
2022-05-05 10:57:26 +0700

    test failed case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -54,13 +54,13 @@ const createServer = async (container) => {
     },
   ]);
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: () => ({
-      value: 'Hello world!',
-    }),
-  });
+  // server.route({
+  //   method: 'GET',
+  //   path: '/',
+  //   handler: () => ({
+  //     value: 'Hello world!',
+  //   }),
+  // });
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
